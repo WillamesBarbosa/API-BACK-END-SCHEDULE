52be4909cc04effb75606251c22736bd915d28c3
Willames <willamessilva1999@gmail.com>
2023-07-19 19:05:54 -0300

    feat: create https server

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const fs = require('fs');
+const https = require('https');
+const cors = require('cors');
+
+const routes = require('./routes');
+
+const textInitialWhenConnect = `
+-----------------------------------------------
+|                                             |
+|              SERVER STARTED                 |
+|                                             |
+| Server is running at https://localhost:3000 |
+|                                             |
+-----------------------------------------------
+`;
+
+// Server config
+const app = express();
+// For convert JSON on Js Object
+app.use(express.json());
+// For minimize erros about cors
+app.use(cors());
+app.use(routes);
+// Middleware so that errors can respond to the client in case of errors
+app.use((error, request, response, next) => {
+  console.log('## ERROR HANDLER ##');
+  console.log(error);
+  response.sendStatus(500);
+});
+
+// For read ssl certified
+const options = {
+  key: fs.readFileSync('ssl/code.key'),
+  cert: fs.readFileSync('ssl/code.crt'),
+};
+
+// Server port
+const port = 3000;
+
+// Function for create the HTTPS server
+const server = https.createServer(options, app);
+
+server.listen(port, () => console.log(textInitialWhenConnect));
