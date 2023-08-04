6dd59b38115223bff227968030d798a87b47afa7
Willames <willamessilva1999@gmail.com>
2023-08-04 16:19:57 -0300

    feat: create new route findAll

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,9 @@
 const Router = require('express');
 
+const ScheduleController = require('./app/controllers/scheduleController');
+
 const router = Router();
 
-router.get('/schedules');
+router.get('/schedules', ScheduleController.findAll);
 
 module.exports = router;
