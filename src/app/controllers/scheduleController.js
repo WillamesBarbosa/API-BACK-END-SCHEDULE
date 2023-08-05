fbd8feefb981f91e7f07af254bee81332574f32c
Willames <willamessilva1999@gmail.com>
2023-08-04 21:48:51 -0300

    refactor: update store and delete

--- src/app/controllers/scheduleController.js
+++ src/app/controllers/scheduleController.js
@@ -13,14 +13,28 @@ class ScheduleController {
     } = request.body;
     const emailAlreadyExists = await SchedulesRepository.findEmail(email);
 
-    if (emailAlreadyExists) {
-      return response.json({ error: 'Email já existe!' });
-    }
-    const paciente = await SchedulesRepository.create({
-      nameComplete, email, passwordUser, verified,
-    });
+    if (!emailAlreadyExists) {
+      const paciente = await SchedulesRepository.create({
+        nameComplete, email, passwordUser, verified,
+      });
 
-    return response.json(paciente);
+      return response.json(paciente);
+    }
+
+    return response.json({ error: 'Email já existe!' });
+  }
+
+  async delete(request, response) {
+    const { id } = request.params;
+
+    const idExist = await SchedulesRepository.delete(id);
+
+    if (!idExist) {
+      return response.status(404).json({ Error: 'Usuário não existe' });
+    }
+
+    await SchedulesRepository.delete(id);
+    return response.sendStatus(204);
   }
 }
 
