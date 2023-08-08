231f4a461c81c957a40d358051eb0ba487f4fb61
Willames <willamessilva1999@gmail.com>
2023-08-08 20:32:26 -0300

    refactor: rename and reposite the routes

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,12 @@ const ScheduleController = require('./app/controllers/userController');
 
 const router = Router();
 
+router.post('/login', ScheduleController.login);
+router.post('/user', ScheduleController.store);
+
 router.get('/users', ScheduleController.index);
-router.get('/users/:id', ScheduleController.show);
-router.post('/users', ScheduleController.store);
-router.post('/users/login', ScheduleController.login);
-router.delete('/users/:id', ScheduleController.delete);
-router.put('/users/:id', ScheduleController.update);
+router.get('/user', ScheduleController.show);
+router.delete('/user/:id', ScheduleController.delete);
+router.put('/user/:id', ScheduleController.update);
 
 module.exports = router;
