566e35d7d659d76275e6b21eaabe84f1d755a855
Willames <willamessilva1999@gmail.com>
2023-08-08 12:39:06 -0300

    feat: create new route for show unique user

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ const ScheduleController = require('./app/controllers/scheduleController');
 const router = Router();
 
 router.get('/schedules', ScheduleController.index);
+router.get('/schedules/:id', ScheduleController.show);
 router.post('/schedules', ScheduleController.store);
 router.delete('/schedules/:id', ScheduleController.delete);
 router.put('/schedules/:id', ScheduleController.update);
