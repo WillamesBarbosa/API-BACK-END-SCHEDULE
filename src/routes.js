7ff103efc3357d0db539ee7e80a81b8deff24532
Willames <willamessilva1999@gmail.com>
2023-08-08 17:35:29 -0300

    feat: create new route login

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ const router = Router();
 router.get('/schedules', ScheduleController.index);
 router.get('/schedules/:id', ScheduleController.show);
 router.post('/schedules', ScheduleController.store);
+router.post('/schedules/login', ScheduleController.login);
 router.delete('/schedules/:id', ScheduleController.delete);
 router.put('/schedules/:id', ScheduleController.update);
 
