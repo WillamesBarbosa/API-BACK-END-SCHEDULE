714c0ac403cf4325027219127a5391c53499a2b3
Willames <willamessilva1999@gmail.com>
2023-08-09 18:23:24 -0300

    feat: include all functions on try catch

--- src/app/controllers/userController.js
+++ src/app/controllers/userController.js
@@ -6,101 +6,123 @@ const SchedulesRepository = require('../repositories/usersRepository');
 
 class ScheduleController {
   async index(request, response) {
-    const schedules = await SchedulesRepository.findAll();
+    try {
+      const schedules = await SchedulesRepository.findAll();
 
-    response.json(schedules);
+      return response.json(schedules);
+    } catch (error) {
+      return response.status(500).json('Ocorreu um erro interno no servidor');
+    }
   }
 
   async show(request, response) {
-    const { id } = request;
-    const user = await SchedulesRepository.findId(id);
+    try {
+      const { id } = request;
+      const user = await SchedulesRepository.findId(id);
 
-    if (!user) {
-      return response.status(404).json({ error: 'Usuário não existe' });
+      if (!user) {
+        return response.status(404).json({ Error: 'Usuário não existe' });
+      }
+
+      return response.json(user);
+    } catch (error) {
+      return response.status(500).json({ Error: 'Ocorreu um erro interno no servidor' });
     }
-
-    return response.json(user);
   }
 
   async login(request, response) {
-    const { email, passwordUser } = request.body;
+    try {
+      const { email, passwordUser } = request.body;
 
-    const user = await SchedulesRepository.findEmail(email);
+      const user = await SchedulesRepository.findEmail(email);
 
-    if (!user) {
-      return response.status(404).json({ error: 'Email não existe!' });
+      if (!user) {
+        return response.status(404).json({ error: 'Email não existe!' });
+      }
+      const passwordAuth = await bcrypt.compareSync(passwordUser, user.passworduser);
+
+      if (!passwordAuth) {
+        return response.status(401).json({ error: 'Senha incorreta!' });
+      }
+
+      const token = jwt.sign({ id: user.id }, process.env.SECRET, { expiresIn: 120 });
+      return response.json(token);
+    } catch (error) {
+      return response.status(500).json({ Error: 'Ocorreu um erro interno no servidor' });
     }
-    const passwordAuth = await bcrypt.compareSync(passwordUser, user.passworduser);
-
-    if (!passwordAuth) {
-      return response.status(401).json({ error: 'Senha incorreta!' });
-    }
-
-    const token = jwt.sign({ id: user.id }, process.env.SECRET, { expiresIn: 120 });
-    return response.json(token);
   }
 
   async store(request, response) {
-    const {
-      nameComplete, email, passwordUser, verified = false,
-    } = request.body;
+    try {
+      const {
+        nameComplete, email, passwordUser, verified = false,
+      } = request.body;
 
-    const emailAlreadyExists = await SchedulesRepository.findEmail(email);
+      const emailAlreadyExists = await SchedulesRepository.findEmail(email);
 
-    const passwordHash = await bcrypt.hash(passwordUser, 15);
+      const passwordHash = await bcrypt.hash(passwordUser, 15);
 
-    if (!emailAlreadyExists) {
-      const paciente = await SchedulesRepository.create({
-        nameComplete, email, passwordHash, verified,
-      });
+      if (!emailAlreadyExists) {
+        const paciente = await SchedulesRepository.create({
+          nameComplete, email, passwordHash, verified,
+        });
 
-      return response.json(paciente);
+        return response.json(paciente);
+      }
+
+      return response.status(409).json({ error: 'Email já existe!' });
+    } catch (error) {
+      return response.status(500).json({ Error: 'Ocorreu um erro interno no servidor' });
     }
-
-    return response.status(409).json({ error: 'Email já existe!' });
   }
 
   async update(request, response) {
-    const {
-      nameComplete, email, passwordUser, verified = false,
-    } = request.body;
-    const { id } = request;
+    try {
+      const {
+        nameComplete, email, passwordUser, verified = false,
+      } = request.body;
+      const { id } = request;
 
-    console.log(request.body);
+      const userExist = await SchedulesRepository.findId(id);
 
-    const userExist = await SchedulesRepository.findId(id);
+      if (!userExist) {
+        return response.status(404).json({ error: 'Usuário não existe!' });
+      }
 
-    if (!userExist) {
-      return response.status(404).json({ error: 'Usuário não existe!' });
+      const paciente = await SchedulesRepository.update(
+        id,
+        {
+          nameComplete,
+
+          email,
+
+          passwordUser,
+
+          verified,
+        },
+      );
+
+      return response.json(paciente);
+    } catch (error) {
+      return response.status(500).json({ Error: 'Ocorreu um erro interno no servidor' });
     }
-
-    const paciente = await SchedulesRepository.update(
-      id,
-      {
-        nameComplete,
-
-        email,
-
-        passwordUser,
-
-        verified,
-      },
-    );
-
-    return response.json(paciente);
   }
 
   async delete(request, response) {
-    const { id } = request;
+    try {
+      const { id } = request;
 
-    const idExist = await SchedulesRepository.delete(id);
+      const idExist = await SchedulesRepository.delete(id);
 
-    if (!idExist) {
-      return response.status(404).json({ Error: 'Usuário não existe' });
+      if (!idExist) {
+        return response.status(404).json({ Error: 'Usuário não existe' });
+      }
+
+      await SchedulesRepository.delete(id);
+      return response.sendStatus(204);
+    } catch (error) {
+      return response.status(500).json({ Error: 'Ocorreu um erro interno no servidor' });
     }
-
-    await SchedulesRepository.delete(id);
-    return response.sendStatus(204);
   }
 }
 
