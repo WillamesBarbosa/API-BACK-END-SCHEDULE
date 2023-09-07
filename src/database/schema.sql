086508c1c08731ef695c180a00a4ab7f8e12e520
Willames <willamessilva1999@gmail.com>
2023-09-06 22:04:30 -0300

    chore: update new schema

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -1,32 +1,68 @@
--- Necessário para gerar UUIDs
 CREATE DATABASE myschedules;
 
+-- Necessário para gerar UUIDs
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE IF NOT EXISTS pacientes (
+-- Tabela de pacientes
+CREATE TABLE IF NOT EXISTS patient (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    nameComplete VARCHAR,
-    email VARCHAR,
-    passwordUser VARCHAR,
-    verified boolean
-    authLevel smallint
+    full_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    street_address VARCHAR(255),
+    city VARCHAR(255),
+    state_province VARCHAR(255),
+    mobile_number VARCHAR(20),
+    authorization_level smallint DEFAULT 0,
+    is_verified boolean DEFAULT false
 );
 
-CREATE TABLE IF NOT EXISTS medico (
+-- Tabela dos médicos
+CREATE TABLE IF NOT EXISTS medic (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    nome VARCHAR NOT NULL,
-    especialidade VARCHAR(50) NOT NULL,
+    full_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    street_address VARCHAR(255),
+    city VARCHAR(255),
+    state_province VARCHAR(255),
+    mobile_number VARCHAR(20),
     crm VARCHAR(20) NOT NULL,
-    telefone VARCHAR(20),
-    email VARCHAR,
-    authLevel smallint,
-    passwordDoctor VARCHAR
+    specialization VARCHAR(255) NOT NULL,
+    authorization_level smallint DEFAULT 1,
+    is_verified boolean DEFAULT false
 );
 
-CREATE TABLE IF NOT EXISTS horarios_disponiveis (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    data_hora TIMESTAMP WITH TIME ZONE,
-    disponivel BOOLEAN NOT NULL DEFAULT TRUE,
-    id_paciente UUID REFERENCES pacientes(id) ON DELETE SET NULL,
-    id_medico UUID REFERENCES medico(id) ON DELETE SET NULL
+-- Tabela dos administradores
+CREATE TABLE IF NOT EXISTS admin (
+   id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    full_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    authorization_level smallint DEFAULT 2,
+    is_verified boolean DEFAULT false
 );
+
+-- Tabela dos horários
+CREATE TABLE IF NOT EXISTS available_times (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    date_time TIMESTAMP WITH TIME ZONE,
+    available BOOLEAN NOT NULL DEFAULT TRUE,
+    id_patient UUID REFERENCES patient(id) ON DELETE SET NULL,
+    id_medic UUID REFERENCES medic(id) ON DELETE SET NULL
+);
+
+-- Tabela de histórico
+CREATE TABLE IF NOT EXISTS medical_history (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    patient_id UUID REFERENCES patient(id) NOT NULL,
+    medic_id UUID REFERENCES medic(id) NOT NULL,
+    consultation_date TIMESTAMP WITH TIME ZONE NOT NULL,
+    diagnosis TEXT,
+    treatment_plan TEXT,
+    prescription TEXT,
+    notes TEXT,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+-- INDÍCES
