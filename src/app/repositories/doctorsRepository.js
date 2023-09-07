614d9519d746e00843bb64f0ee001f747eae3906
Willames <willamessilva1999@gmail.com>
2023-09-06 23:42:53 -0300

    feat: create DoctorRepository and function create

---
+++ src/app/repositories/doctorsRepository.js
@@ -0,0 +1,46 @@
+const database = require('../../database/index');
+
+class DoctorsRepository {
+  async create({
+    full_name,
+    email,
+    password_hash,
+    street_address,
+    city,
+    state_province,
+    mobile_number,
+    crm,
+    specialization,
+  }) {
+    const rows = await database.query(
+      `
+      INSERT INTO medic(full_name,
+        email,
+        password_hash,
+        street_address,
+        city,
+        state_province,
+        mobile_number,
+        crm,
+        specialization)
+      VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)
+      RETURNING *
+    `,
+      [
+        full_name,
+        email,
+        password_hash,
+        street_address,
+        city,
+        state_province,
+        mobile_number,
+        crm,
+        specialization,
+      ],
+    );
+
+    return rows;
+  }
+}
+
+module.exports = new DoctorsRepository();
