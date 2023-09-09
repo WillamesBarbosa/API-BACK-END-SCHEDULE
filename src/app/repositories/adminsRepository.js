cb6a6520df2d97ec58063f0abe067b3c6ee76390
Willames <willamessilva1999@gmail.com>
2023-09-09 14:17:56 -0300

    feat: create function findAll into adminsRepository

--- src/app/repositories/adminsRepository.js
+++ src/app/repositories/adminsRepository.js
@@ -1,6 +1,15 @@
 const database = require('../../database/index');
 
 class AdminsRepository {
+  async findAll() {
+    const rows = await database.query(`
+    SELECT *
+    FROM admin
+    `);
+
+    return rows;
+  }
+
   async create({ full_name, email, password_hash }) {
     const [rows] = await database.query(`
     INSERT INTO admin(full_name, email, password_hash)
