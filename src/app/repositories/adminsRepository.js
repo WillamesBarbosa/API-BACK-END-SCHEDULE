35367a603791fa039339745a9c8a45ba72ecabb1
Willames <willamessilva1999@gmail.com>
2023-09-09 12:29:33 -0300

    feat: create adminsRepository and function index

---
+++ src/app/repositories/adminsRepository.js
@@ -0,0 +1,15 @@
+const database = require('../../database/index');
+
+class AdminsRepository {
+  async create({ full_name, email, password_hash }) {
+    const [rows] = await database.query(`
+    INSERT INTO admin(full_name, email, password_hash)
+    VALUES($1, $2, $3)
+    RETURNING *
+    `, [full_name, email, password_hash]);
+
+    return rows;
+  }
+}
+
+module.exports = new AdminsRepository();
