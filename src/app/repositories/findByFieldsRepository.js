2400a40b709b295f5f550d9995d0678b2015751b
Willames <willamessilva1999@gmail.com>
2023-09-13 08:59:45 -0300

    feat: wrap findByField in trycatch block to catch errors

--- src/app/repositories/findByFieldsRepository.js
+++ src/app/repositories/findByFieldsRepository.js
@@ -1,14 +1,20 @@
 const database = require('../../database/index');
+const logger = require('../logger/winston');
 
 class FindByField {
   async findByField(table, field, value) {
-    const [row] = await database.query(`
-          SELECT *
-          FROM ${table}
-          WHERE ${field} = $1
-        `, [value]);
+    try {
+      const [row] = await database.query(`
+            SELECT *
+            FROM ${table}
+            WHERE ${field} = $1
+          `, [value]);
 
-    return row;
+      return row;
+    } catch (error) {
+      logger.error('Erro no findByFieldRepository', error);
+      throw new Error('Ocorreu um erro durante uma consulta ao banco de dados.');
+    }
   }
 }
 
