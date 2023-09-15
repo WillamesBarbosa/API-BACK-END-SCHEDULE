4ea1b69fb504bd4859aa63800c2e9ea231bfef36
Willames <willamessilva1999@gmail.com>
2023-09-15 20:36:04 -0300

    feat: use luxo to format hours into logs

--- src/app/logger/winston.js
+++ src/app/logger/winston.js
@@ -1,11 +1,19 @@
 const { createLogger, format, transports } = require('winston');
+const { DateTime } = require('luxon');
 require('dotenv').config();
 
 const logger = createLogger({
   level: 'info', // Nível mínimo de log para produção (por exemplo, 'info' ou 'error')
   format: format.combine(
-    format.timestamp(),
-    format.json(), // Definindo o formato JSON para facilitar a análise automatizada
+    format.timestamp({
+      format: () => {
+        // Cria uma instância e define o fuso horário de São Paulo
+        const brasiliaTime = DateTime.now().setZone('America/Sao_Paulo');
+        // Formata para o padrão ISO 8601
+        return brasiliaTime.toISO({ includeOffset: false });
+      },
+    }),
+    format.json(),
   ),
   transports: [
     new transports.File({ filename: 'logs/error.log', level: 'error' }), // Registra erros em um arquivo separado
