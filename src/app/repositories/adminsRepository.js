394d26f26c12ef3be2ad8be69d45b8c88882321d
Willames <willamessilva1999@gmail.com>
2023-09-15 18:33:12 -0300

    feat: create function update into AdminsRepository

--- src/app/repositories/adminsRepository.js
+++ src/app/repositories/adminsRepository.js
@@ -19,6 +19,34 @@ class AdminsRepository {
 
     return rows;
   }
+
+  async update(
+    {
+      full_name,
+      password,
+      email,
+    },
+    id,
+  ) {
+    const [rows] = await database.query(
+      `
+      UPDATE admin
+      SET full_name = $1,
+      email = $2,
+      password_hash = $3
+      WHERE id = $4
+      RETURNING *
+      `,
+      [
+        full_name,
+        email,
+        password,
+        id,
+      ],
+    );
+    console.log(rows);
+    return rows;
+  }
 }
 
 module.exports = new AdminsRepository();
