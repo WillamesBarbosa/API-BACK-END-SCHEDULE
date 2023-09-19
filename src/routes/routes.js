fa5717df7750573e81dabfd9df18fbd487f875da
Willames <willamessilva1999@gmail.com>
2023-09-19 12:46:40 -0300

    feat: finalize routes that allow CRUD of people and their access middleware

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,21 +1,41 @@
 const Router = require('express');
 
-const userController = require('../app/controllers/userController');
-const doctorController = require('../app/controllers/doctorController');
+const UserController = require('../app/controllers/userController');
+const DoctorController = require('../app/controllers/doctorController');
+const Login = require('../app/controllers/loginController');
+const AdminController = require('../app/controllers/adminController');
 
 const { authenticationMiddleware } = require('../middlewares/authenticationMiddleware');
+const { authorizationMiddlewareLevel1 } = require('../middlewares/authorizationMiddlewareLevel1');
+const { authorizationMiddlewareLevel2 } = require('../middlewares/authorizationMiddlewareLevel2');
+const { errorMiddleware } = require('../middlewares/errorMiddleware');
 
 const router = Router();
 
-router.post('/login', userController.login);
-router.post('/user', userController.store);
-router.post('/doctor', doctorController.store);
+router.post('/login', Login.login);
+router.post('/user', UserController.store);
 
 router.use(authenticationMiddleware);
 
-router.get('/users', userController.index);
-router.get('/user', userController.show);
-router.delete('/user', userController.delete);
-router.put('/user', userController.update);
+router.get('/user', UserController.show);
+router.put('/user', UserController.update);
 
+router.use(authorizationMiddlewareLevel1);
+router.get('/doctor', DoctorController.show);
+router.put('/doctor', DoctorController.update);
+
+router.use(authorizationMiddlewareLevel2);
+router.get('/admin', AdminController.show);
+
+router.get('/admins', AdminController.index);
+router.post('/admin', AdminController.store);
+router.put('/admin', AdminController.update);
+router.delete('/admin', AdminController.delete);
+router.post('/doctor', DoctorController.store);
+router.get('/doctors', DoctorController.index);
+router.get('/users', UserController.index);
+router.delete('/user', UserController.delete);
+router.delete('/doctor', DoctorController.delete);
+
+router.use(errorMiddleware);
 module.exports = router;
