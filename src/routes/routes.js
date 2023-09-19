962719df0c29f369089d94a388765a2efc686d29
Willames <willamessilva1999@gmail.com>
2023-09-19 12:56:25 -0300

    docs: document all routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -12,30 +12,47 @@ const { errorMiddleware } = require('../middlewares/errorMiddleware');
 
 const router = Router();
 
+// Rotas públicas
 router.post('/login', Login.login);
 router.post('/user', UserController.store);
 
+// Middleware que verifica o Token JWT
 router.use(authenticationMiddleware);
 
+// Rotas que apenas usuários autenticados e níveis de autorização maior ou igual 0 tem acesso.
+// Rotas dos pacientes
 router.get('/user', UserController.show);
 router.put('/user', UserController.update);
 
+// Middleware que permite apenas que a partir daqui apenas usuários
+// com nível de autorização 1 ou maior tenham acesso
 router.use(authorizationMiddlewareLevel1);
+
+// Rotas dos médicos
 router.get('/doctor', DoctorController.show);
 router.put('/doctor', DoctorController.update);
 
+// Middleware que permite apenas que a partir daqui apenas usuários
+// com nível de autorização 2 ou maior tenham acessorouter.use(authorizationMiddlewareLevel2);
 router.use(authorizationMiddlewareLevel2);
-router.get('/admin', AdminController.show);
 
+// Rotas exclusivas para administradores
+
+// Admin
 router.get('/admins', AdminController.index);
+router.get('/admin', AdminController.show);
 router.post('/admin', AdminController.store);
 router.put('/admin', AdminController.update);
 router.delete('/admin', AdminController.delete);
-router.post('/doctor', DoctorController.store);
+
+// Médico
 router.get('/doctors', DoctorController.index);
+router.post('/doctor', DoctorController.store);
+router.delete('/doctor', DoctorController.delete);
+
+// Paciente
 router.get('/users', UserController.index);
 router.delete('/user', UserController.delete);
-router.delete('/doctor', DoctorController.delete);
 
 router.use(errorMiddleware);
 module.exports = router;
